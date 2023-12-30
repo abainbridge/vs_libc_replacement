f212d3879e8128aa119255982b023e442fd057f7
Andrew Bainbridge <2005@deadfrog.co.uk>
2023-12-30 09:50:18 +0000

    Add support for new/delete in C++ applications.

---
+++ src/cpp_support.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+
+void * __cdecl operator new(size_t bytes) {
+    return malloc(bytes);
+}
+
+void * __cdecl operator new[](size_t bytes) {
+    return malloc(bytes);
+}
+
+void __cdecl operator delete(void *ptr) {
+    free(ptr);
+}
+
+void __cdecl operator delete[](void *ptr) {
+    free(ptr);
+}
